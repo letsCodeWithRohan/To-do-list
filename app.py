435e703b1074e2a3e964f143599a83567adf1926
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-08-17 12:43:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config["MYSQL_HOST"] = 'localhost'
-app.config["MYSQL_USER"] = 'root'
-app.config["MYSQL_PASSWORD"] = ''
-app.config["MYSQL_DB"] = 'posts'
+app.config["MYSQL_HOST"] = 'localhost' # Your Host address
+app.config["MYSQL_USER"] = 'root' # Your Username
+app.config["MYSQL_PASSWORD"] = '' # Your Password
+app.config["MYSQL_DB"] = 'to_do_list' # Your database name 
 
 mysql = MySQL(app)
 
